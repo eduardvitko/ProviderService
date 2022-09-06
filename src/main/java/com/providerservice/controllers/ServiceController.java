5c3cdbacfcdc54daeb333f50c6374097219bea0c
eduar <eduardvitko>
2022-09-06 22:08:30 +0300

    create method getAllServices in ServiceController

--- src/main/java/com/providerservice/controllers/ServiceController.java
+++ src/main/java/com/providerservice/controllers/ServiceController.java
@@ -1,14 +1,15 @@
 package com.providerservice.controllers;
 
+import com.providerservice.model.CustomerEntity;
 import com.providerservice.model.ServiceEntity;
 import com.providerservice.serviceImplements.ServiceServiceImpl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/service-service")
@@ -18,5 +19,10 @@ public class ServiceController {
     @PostMapping("/service/create")
     public ResponseEntity<ServiceEntity> createService(@RequestBody ServiceEntity serviceEntity) {
         return ResponseEntity.ok(serviceService.createService(serviceEntity));
+    }
+    @GetMapping("/service/getAllServices")
+    public ResponseEntity<List<ServiceEntity>> getAllServices() {
+        List<ServiceEntity> services = serviceService.findAll();
+        return new ResponseEntity<>(services, OK);
     }
 }
