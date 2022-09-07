115559651d64cfa84decc176c74b5aa28316614b
eduar <eduardvitko>
2022-09-07 22:29:42 +0300

    create class JWTTokenProvider

---
+++ src/main/java/com/providerservice/filter/JWTTokenProvider.java
@@ -0,0 +1,35 @@
+package com.providerservice.filter;
+
+import com.auth0.jwt.JWT;
+import com.providerservice.model.UserPrincipal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.providerservice.constant.SecurityConstant.*;
+
+@Component
+public class JWTTokenProvider {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private String[] getClaimsFromUser(UserPrincipal user) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : user.getAuthorities()){
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
+    }
+    public String generateJwtToken(UserPrincipal userPrincipal) {
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create().withIssuer(GET_ARRAYS_LLC).withAudience(GET_ARRAYS_ADMINISTRATION)
+                .withIssuedAt(new Date()).withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims).withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(secret.getBytes()));
+    }
+}
