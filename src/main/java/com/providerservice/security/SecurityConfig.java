ee2502103ac47c716f037bad3cdfdaebef9c99a1
eduar <eduardvitko>
2022-10-08 14:17:44 +0300

    fix AuthentificationFilter

--- src/main/java/com/providerservice/security/SecurityConfig.java
+++ src/main/java/com/providerservice/security/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.providerservice.security;
 
 import com.providerservice.filter.AuthentificationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.Resource;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 
 @Configuration
@@ -36,10 +38,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        AuthentificationFilter authentificationFilter = new AuthentificationFilter(authenticationManagerBean());
+        authentificationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new AuthentificationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers("api/login/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/users/**").hasAnyAuthority("CLIENT");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(authentificationFilter);
     }
 
     @Bean
